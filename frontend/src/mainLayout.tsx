7c6f2d559e7f5162b6edc30d98d14c41e04560b7
moatazemadnaeem <moatazemad772@gmail.com>
2024-12-04 18:25:53 +0200

    (frontend) added spin for re-fresh

--- frontend/src/mainLayout.tsx
+++ frontend/src/mainLayout.tsx
@@ -1,7 +1,17 @@
 import NavBar from "./components/navbar";
 import { Outlet } from "react-router-dom";
+import { useAuth } from "./hooks/userAuth";
+import { Spin } from "antd";
 
 function MainLayout() {
+  const { loading } = useAuth();
+  if (loading) {
+    return (
+      <div className="glob-spin">
+        <Spin size="large" />
+      </div>
+    );
+  }
   return (
     <>
       <NavBar />
