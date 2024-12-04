0ecea0647b9b08c17572c442724c1976faab4203
moatazemadnaeem <moatazemad772@gmail.com>
2024-12-04 13:08:49 +0200

    (frontend) initial setup signup & signin pages

--- frontend/src/pages/SignIn/index.tsx
+++ frontend/src/pages/SignIn/index.tsx
@@ -1,5 +1,56 @@
-function SignIn() {
-  return <div>SignIn</div>;
-}
+import { Form, Input, Button } from "antd";
+import { Link } from "react-router-dom";
+import "./signin.css";
+import { validateEmail, validatePassword } from "../../utils/validations";
+const SignIn = () => {
+  const handleSignInApi = async (values: any) => {
+    console.log(values);
+  };
+  return (
+    <div className="container-form">
+      <Form
+        className="signin"
+        name="signin_form"
+        layout="vertical"
+        onFinish={handleSignInApi}
+      >
+        <Form.Item
+          className="email"
+          name="email"
+          label="Email"
+          rules={[
+            {
+              validator: validateEmail,
+            },
+          ]}
+        >
+          <Input />
+        </Form.Item>
+
+        <Form.Item
+          className="password"
+          name="password"
+          label="Password"
+          rules={[
+            {
+              validator: validatePassword,
+            },
+          ]}
+        >
+          <Input.Password />
+        </Form.Item>
+        <Form.Item className="link">
+          <span>Do Not Have an account? </span>
+          <Link to="/signup">SignUp</Link>
+        </Form.Item>
+        <Form.Item className="button">
+          <Button type="primary" htmlType="submit">
+            Sign In
+          </Button>
+        </Form.Item>
+      </Form>
+    </div>
+  );
+};
 
 export default SignIn;
--- frontend/src/pages/SignUp/index.tsx
+++ frontend/src/pages/SignUp/index.tsx
@@ -1,5 +1,73 @@
-function SignUp() {
-  return <div>SignUp</div>;
-}
+import { Form, Input, Button } from "antd";
+import { Link } from "react-router-dom";
+import "./signup.css";
+import {
+  validateName,
+  validateEmail,
+  validatePassword,
+} from "../../utils/validations";
+const SignUp = () => {
+  const handleSignUpApi = async (values: any) => {
+    console.log(values);
+  };
+  return (
+    <div className="container-form">
+      <Form
+        className="signup"
+        name="signup_form"
+        layout="vertical"
+        onFinish={handleSignUpApi}
+      >
+        <Form.Item
+          className="name"
+          name="name"
+          label="Name"
+          rules={[
+            {
+              validator: validateName,
+            },
+          ]}
+        >
+          <Input />
+        </Form.Item>
+
+        <Form.Item
+          className="email"
+          name="email"
+          label="Email"
+          rules={[
+            {
+              validator: validateEmail,
+            },
+          ]}
+        >
+          <Input />
+        </Form.Item>
+
+        <Form.Item
+          className="password"
+          name="password"
+          label="Password"
+          rules={[
+            {
+              validator: validatePassword,
+            },
+          ]}
+        >
+          <Input.Password />
+        </Form.Item>
+        <Form.Item className="link">
+          <span>Already Have An Account? </span>
+          <Link to="/signin">SignIn</Link>
+        </Form.Item>
+        <Form.Item className="button">
+          <Button type="primary" htmlType="submit">
+            Sign Up
+          </Button>
+        </Form.Item>
+      </Form>
+    </div>
+  );
+};
 
 export default SignUp;
